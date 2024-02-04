49cb6f78e24f2e97ba5455bae1838dfe80c708d2
Augusto <gtoborges@gmail.com>
2024-02-04 15:48:32 -0300

    add header link and useParams on details component

--- fem-adoptme/src/Pet.jsx
+++ fem-adoptme/src/Pet.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom"
-const Pet = ({name, animal, breed, images, location, id}) => {
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
-  if(images.length) hero = images[0]
+import { Link } from "react-router-dom";
+
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) hero = images[0];
 
   return (
     <Link to={`/details/${id}`} className="pet">
@@ -10,11 +11,12 @@ const Pet = ({name, animal, breed, images, location, id}) => {
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{animal} - {breed} - {location}</h2>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
       </div>
     </Link>
-  )
-}
+  );
+};
 
 export default Pet;
-
